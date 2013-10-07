577dd84ddd5186aa9b5582a886c5d185d6ca68b0
Rolf Bislin <romibi2@gmail.com>
2013-10-07 16:27:52 +0200

    ups 2 mains

--- testate/testat_1/src/Test.cpp
+++ testate/testat_1/src/Test.cpp
@@ -16,10 +16,10 @@ void runAllTests(int argc, char const *argv[]){
 	cute::makeRunner(lis,argc,argv)(s, "AllTests");
 }
 
-int main(int argc, char const *argv[]){
-    runAllTests(argc,argv);
-    return 0;
-}
+//int main(int argc, char const *argv[]){
+//    runAllTests(argc,argv);
+//    return 0;
+//}
 
 
 
