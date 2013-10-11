f9a356b45f06c63d7167fb8bbd70ec9890cb09d2
Rolf Bislin <romibi2@gmail.com>
2013-10-11 16:43:58 +0200

    Added calc-tests

--- testate/testat_1/src/Test.cpp
+++ testate/testat_1/src/Test.cpp
@@ -3,6 +3,7 @@
 #include "xml_listener.h"
 #include "cute_runner.h"
 #include "sevenSegment.h"
+#include "calc.h"
 #include <iostream>
 #include <sstream>
 
@@ -78,6 +79,97 @@ void testAnotherInputInvalid() {
 	ASSERT_EQUAL(str,os.str());
 }
 
+void testAdd() {
+	ASSERT_EQUAL(8,add(3,5));
+	ASSERT_EQUAL(3,add(0,3));
+	ASSERT_EQUAL(7,add(7,0));
+	ASSERT_EQUAL(0,add(0,0));
+	ASSERT_EQUAL(3,add(-1,4));
+	ASSERT_EQUAL(2,add(6,-4));
+}
+
+void testSub() {
+	ASSERT_EQUAL(-2,sub(3,5));
+	ASSERT_EQUAL(-3,sub(0,3));
+	ASSERT_EQUAL(7,sub(7,0));
+	ASSERT_EQUAL(0,sub(0,0));
+	ASSERT_EQUAL(-5,sub(-1,4));
+	ASSERT_EQUAL(10,sub(6,-4));
+	ASSERT_EQUAL(2,sub(5,3));
+	ASSERT_EQUAL(3,sub(3,0));
+	ASSERT_EQUAL(-7,sub(0,7));
+	ASSERT_EQUAL(5,sub(4,-1));
+	ASSERT_EQUAL(-10,sub(-4,6));
+}
+
+void testMult() {
+	ASSERT_EQUAL(15,mult(3,5));
+	ASSERT_EQUAL(0,mult(0,3));
+	ASSERT_EQUAL(0,mult(7,0));
+	ASSERT_EQUAL(0,mult(0,0));
+	ASSERT_EQUAL(-4,mult(-1,4));
+	ASSERT_EQUAL(-24,mult(6,-4));
+}
+
+void testDivide() {
+	ASSERT_EQUAL(0,divide(3,5));
+	ASSERT_EQUAL(0,divide(0,3));
+	//ASSERT_EQUAL(0,divide(7,0));
+	//ASSERT_EQUAL(0,divide(0,0));
+	ASSERT_EQUAL(0,divide(-1,4));
+	ASSERT_EQUAL(-1,divide(6,-4));
+	ASSERT_EQUAL(1,divide(5,3));
+	//ASSERT_EQUAL(3,divide(3,0));
+	ASSERT_EQUAL(0,divide(0,7));
+	ASSERT_EQUAL(-4,divide(4,-1));
+	ASSERT_EQUAL(0,divide(-4,6));
+}
+
+void testMod() {
+	ASSERT_EQUAL(3,mod(3,5));
+	ASSERT_EQUAL(0,mod(0,3));
+	//ASSERT_EQUAL(0,mod(7,0));
+	//ASSERT_EQUAL(0,mod(0,0));
+	ASSERT_EQUAL(-1,mod(-1,4));
+	ASSERT_EQUAL(2,mod(6,-4));
+	ASSERT_EQUAL(2,mod(5,3));
+	//ASSERT_EQUAL(3,mod(3,0));
+	ASSERT_EQUAL(0,mod(0,7));
+	ASSERT_EQUAL(0,mod(4,-1));
+	ASSERT_EQUAL(-4,mod(-4,6));
+}
+
+void testCheckOperatorAndCalc() {
+	ASSERT_EQUAL("19", checkOperatorAndCalc(12,'+',7).str());
+	ASSERT_EQUAL("5", checkOperatorAndCalc(12,'-',7).str());
+	ASSERT_EQUAL("84", checkOperatorAndCalc(12,'*',7).str());
+	ASSERT_EQUAL("1", checkOperatorAndCalc(12,'/',7).str());
+	ASSERT_EQUAL("Error", checkOperatorAndCalc(12,'/',0).str());
+	ASSERT_EQUAL("5", checkOperatorAndCalc(12,'%',7).str());
+	ASSERT_EQUAL("Error", checkOperatorAndCalc(12,'%',0).str());
+}
+
+void testCalc() {
+	std::stringstream ss1;
+	ss1 << "123+456";
+	ASSERT_EQUAL("579", calc(ss1).str());
+
+	std::stringstream ss2;
+	ss2 << "456/123";
+	ASSERT_EQUAL("3", calc(ss2).str());
+
+	std::stringstream ss3;
+	ss3 << "123 % 456";
+	ASSERT_EQUAL("123", calc(ss3).str());
+
+	std::stringstream ss4;
+	ss4 << "123 456 %";
+	ASSERT_EQUAL("Error", calc(ss4).str());
+
+	std::stringstream ss5;
+	ss5 << "-12 / 6";
+	ASSERT_EQUAL("Error", calc(ss4).str());
+}
 
 void runAllTests(int argc, char const *argv[]){
 	cute::suite s;
@@ -90,6 +182,13 @@ void runAllTests(int argc, char const *argv[]){
 	s.push_back(CUTE(testInputNegative));
 	s.push_back(CUTE(testInputInvalid));
 	s.push_back(CUTE(testAnotherInputInvalid));
+	s.push_back(CUTE(testAdd));
+	s.push_back(CUTE(testSub));
+	s.push_back(CUTE(testMult));
+	s.push_back(CUTE(testDivide));
+	s.push_back(CUTE(testMod));
+	s.push_back(CUTE(testCalc));
+	s.push_back(CUTE(testCheckOperatorAndCalc));
 	cute::xml_file_opener xmlfile(argc,argv);
 	cute::xml_listener<cute::ide_listener<> >  lis(xmlfile.out);
 	cute::makeRunner(lis,argc,argv)(s, "AllTests");
--- testate/testat_1/src/calc.h
+++ testate/testat_1/src/calc.h
@@ -4,6 +4,12 @@
 #include <iosfwd>
 #include <iostream>
 
+int add(int a, int b);
+int sub(int a, int b);
+int mult(int a, int b);
+int divide(int a, int b);
+int mod(int a, int b);
+std::stringstream checkOperatorAndCalc(int a, char op, int b);
 std::stringstream calc(std::istream &in);
 
 #endif /* CALC_H_ */
