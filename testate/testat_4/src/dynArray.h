ed8d9ef6a1ef7b5d604983addeb6421524d066e6
skanon <nosferatu.alucard@hotmail.com>
2013-11-25 15:14:25 +0100

    Testat 4 Initial Commit

---
+++ testate/testat_4/src/dynArray.h
@@ -0,0 +1,20 @@
+
+#ifndef DYNARRAY_H_
+#define DYNARRAY_H_
+
+#include <vector>
+
+template <typename T>
+class dynArray {
+
+	using vector=std::vector<T>;
+	vector v;
+
+public:
+	dynArray() = default;
+
+
+};
+
+
+#endif /* DYNARRAY_H_ */
