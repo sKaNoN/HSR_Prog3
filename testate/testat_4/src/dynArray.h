6b87a023b7657c22aeea3bd884b02af92021ee1c
skanon <nosferatu.alucard@hotmail.com>
2013-11-25 15:26:36 +0100

    member function Size() and at() + not to implement member functions

--- testate/testat_4/src/dynArray.h
+++ testate/testat_4/src/dynArray.h
@@ -1,20 +1,35 @@
-
 #ifndef DYNARRAY_H_
 #define DYNARRAY_H_
 
 #include <vector>
 
-template <typename T>
+template<typename T>
 class dynArray {
 
 	using vector=std::vector<T>;
 	vector v;
 
 public:
+	using size_type = typename vector::size_type;
+	using iterator = typename vector::iterator;
+	using const_iterator = typename vector::const_iterator;
+	using reverse_iterator = typename vector::reverse_iterator;
+	using const_reverse_iterator = typename vector::const_reverse_iterator;
+
 	dynArray() = default;
 
 
-};
 
+
+	size_type size() const {
+		return v.size();
+	}
+
+	T const& at(size_type index) const {
+		if (index<0) return v.at(size()+index);
+		else return v.at(index);
+	}
+
+};
 
 #endif /* DYNARRAY_H_ */
